b9661aab28e272e8f5b7148c0e8af0e6272a4d64
MdSameer-1 <sameermd123xyz@gmail.com>
2026-07-27 23:25:43 +0530

    Time: 5 ms (27.87%), Space: 47.1 MB (39.91%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
@@ -2,9 +2,11 @@ class Solution {
     public int minOperations(int[] nums) {
         int c=0;
         for(int i=0;i<nums.length-1;i++){
-            while(nums[i]>=nums[i+1]){
-                nums[i+1]++;
-                c++;
+            int d=0;
+            if(nums[i]>=nums[i+1]){
+                d=nums[i]-nums[i+1]+1;
+                c+=d;
+                nums[i+1]+=d;
             }
         }
         return c;
